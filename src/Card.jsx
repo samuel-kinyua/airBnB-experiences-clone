63206a11179c83787d9a7c6b663fb1042b7c639d
sam <samuelkinyua722@gmail.com>
2023-10-19 10:42:33 +0300

    add the card badge to indicate when the location is online and when it is sold out

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default function Card(props){
+	let floatingBadge
+	if(props.item.openSpots === 0){
+		floatingBadge = "Sold out"
+	}else if( props.item.location === "Online"){
+		floatingBadge = 'Online'
+	}
 	return(
 		<div className='card'>
 			<img className="card--image" src={`/assets/${props.item.coverImg}`} />
+				{floatingBadge && <div className='card--badge'>{floatingBadge}</div>}
 			<div className="card--text">
 				<div className="stats">
 					<img className="rating--star" src={"/assets/star.png"} />
